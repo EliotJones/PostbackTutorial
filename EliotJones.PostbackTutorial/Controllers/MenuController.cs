67f816e825435c0ae3c71365acfdb53ab855a72d
Eliot Jones <elioty@hotmail.co.uk>
2015-06-18 21:50:59 +0100

    Add initial form

--- EliotJones.PostbackTutorial/Controllers/MenuController.cs
+++ EliotJones.PostbackTutorial/Controllers/MenuController.cs
@@ -1,13 +1,74 @@
 ﻿namespace EliotJones.PostbackTutorial.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
+    using Models;
+    using ViewModels.Menu;
 
     public class MenuController : Controller
     {
+        private const string MenuSessionKey = "menu";
+
+        private Menu Menu
+        {
+            get
+            {
+                if (Session[MenuSessionKey] == null)
+                {
+                    Session[MenuSessionKey] = new Menu();
+                }
+
+                return (Menu) Session[MenuSessionKey];
+            }
+        }
+
         [HttpGet]
         public ActionResult Order()
         {
+            var model = new OrderViewModel
+            {
+                MenuItems = GetSelectListOfMenu()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Order(OrderViewModel model)
+        {
+            model.MenuItems = GetSelectListOfMenu();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            Meal meal = Menu.Single(m => m.Id == model.SelectedMenuItem);
+
+            Menu.Order(meal);
+
+            return RedirectToAction("Summary", new { name = meal.Name, time = meal.TimeToServe });
+        }
+
+        [HttpGet]
+        public ActionResult Summary(string name, string time)
+        {
+            ViewBag.Message = "You ordered " + name + " successfully. "
+                              + "The meal will be ready in " + time + " minutes.";
             return View();
+        }
+
+        protected virtual SelectList GetSelectListOfMenu()
+        {
+            return new SelectList(Menu, "Id", "Name");
+        }
+
+        [HttpGet]
+        public ActionResult Clear()
+        {
+            Session[MenuSessionKey] = null;
+
+            return RedirectToAction("Order");
         }
     }
 }
---
+++ EliotJones.PostbackTutorial/ViewModels/Menu/OrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace EliotJones.PostbackTutorial.ViewModels.Menu
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class OrderViewModel
+    {
+        public SelectList MenuItems { get; set; }
+
+        [Required(ErrorMessage = "Please select a menu item.")]
+        public int? SelectedMenuItem { get; set; }
+
+        public IList<int> SelectedItems { get; set; }
+
+        public OrderViewModel()
+        {
+            this.SelectedItems = new List<int>();
+        }
+    }
+}
